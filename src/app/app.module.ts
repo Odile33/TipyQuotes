dca0e7d180ebf31ba2df5350e957b17039022561
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-01 19:59:47 +0200

    Added the side panel components and quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidepanelComponent } from './sidepanel/sidepanel.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidepanelComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
